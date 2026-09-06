96bf49ba32d36e4aa3464a01eca33e33e9a53ca5
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-06 21:14:31 +0100

    Update variable & display when digits are pressed
    First attempt at updating the display as buttons are clicked.
    
    The operands need to be stored so `firstNumber` is used as a test for
    the look of that functionality.

--- js/script.js
+++ js/script.js
@@ -3,10 +3,11 @@ const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
 
-let firstNumber;
-let secondNumber;
+let firstNumber = "";
+let secondNumber = "";
 let operator;
 
+// execute one maths operation
 function operate(operator, firstNumber, secondNumber) {
     const x = parseInt(firstNumber);
     const y = parseInt(secondNumber);
@@ -22,4 +23,37 @@ function operate(operator, firstNumber, secondNumber) {
             return divide(x, y);
     }
     return "operator unknown";
+}
+
+// map digit buttons' id values to actual digits
+const numbers = {
+    "seven": "7",
+    "eight": "8",
+    "nine": "9",
+    "four": "4",
+    "five": "5",
+    "six": "6",
+    "one": "1",
+    "two": "2",
+    "three": "3",
+    "zero": "0",
+};
+
+const numpad = document.querySelector(".numpad");
+numpad.addEventListener("click", receiveInput);
+
+function receiveInput(event) {
+    let target = event.target.id;
+    
+    // ensure only digit buttons are picked up
+    if (numbers[target] !== undefined) {
+        firstNumber += numbers[target];
+    }
+    
+    updateDisplay(firstNumber);
+}
+
+function updateDisplay(output) {
+    const display = document.querySelector(".display");
+    display.textContent = output;
 }
