ec63f6f7b37252077eaeedb58232f4259d7f2c86
Fortunatus Olorunsola <fortunatus.olorunsola@gmail.com>
2026-09-06 10:15:53 +0100

    Add function to execute operation

--- js/script.js
+++ js/script.js
@@ -2,3 +2,26 @@ const add = (x, y) => x + y;
 const subtract = (x, y) => x - y;
 const multiply = (x, y) => x * y;
 const divide = (x, y) => x / y;
+
+let firstNumber;
+let secondNumber;
+let operator;
+
+function operate(operator, firstNumber, secondNumber) {
+    const x = parseInt(firstNumber);
+    const y = parseInt(secondNumber);
+
+    switch(operator) {
+        case "+":
+            return add(x, y);
+        case "-":
+            return subtract(x, y);
+        case "*":
+            return multiply(x, y);
+        case "/":
+            return divide(x, y);
+    }
+    return "operator unknown";
+}
+
+console.log(operate("*", "3", "9")); // test
